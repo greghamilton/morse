b7d66596cea80db04c9f84015c0bb58e360dde3e
Greg Hamilton <gregregreg@gmail.com>
2022-05-05 19:00:24 +1000

    Initial commit.

---
+++ timer.asm
@@ -0,0 +1,41 @@
+;
+;
+; ..\dasm-2.20.14.1-win-x64\dasm.exe timer.asm -f3 -v5 -otimer.bin
+;
+;
+
+    processor   6803
+
+TCSR    equ $08
+TIMER   equ $09
+TOCR    equ $0B
+TICR    equ $0D   
+; set bit 2 of TCSR to enable OCF
+
+    org     $4E00
+
+start
+    ldd     #$0800
+    std     delay
+    ldaa    #126
+    staa    16902
+    ldx     #timer_overflow
+    stx     16903
+    cli
+    ldaa    #0
+    staa    TCSR
+    rts
+
+timer_overflow
+    ldaa    sound
+    eora    #$80
+    staa    sound
+    staa    49151
+    ldd     delay
+    addd    TIMER
+    bita    TCSR
+    std     TOCR
+    rti
+
+sound   ds 1
+delay   ds 2
